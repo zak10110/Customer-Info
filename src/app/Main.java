a4a4c6d88b292093283608662056d91980b0f2e6
zak10110 <zr380678644997@gmail.com>
2024-06-17 17:31:12 +0300

    Add implementation of Customer class and its usage in Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,22 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Customer Info app");
+        Customer customer = getCustomer(getData());
+        String output = "Customer: " + customer.getName()+", phone " + customer.getPhone();
+        getOutput(output);
+    }
+
+    public static String[] getData() {
+        return new String[]{"Tom","555 123-8596"};
+    }
+
+    public static Customer getCustomer(String[] data) {
+        return new Customer(data[0], data[1]);
+    }
+
+    public static void getOutput(String output) {
+        System.out.println(output);
     }
 }
